2b6753f52eb50826320f09cc5c1d5d6a8cd0db40
estivensal7 <estivensal7@gmail.com>
2020-09-02 17:58:46 -0400

    Added token signing to register route && created the jwt auth middleware function to verify the tokens.

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator/check");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
+const config = require("config");
 
 // @route       POST api/users
 // @desc        Register user
@@ -59,8 +61,22 @@ router.post(
 
 			await user.save();
 
-			// Return jsonwebtoken
-			res.send("User registered.");
+			// JSON Web Token
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"),
+				{ expiresIn: 360000 },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
 		} catch (err) {
 			console.log(err.message);
 			res.status(500).send("Server error.");
